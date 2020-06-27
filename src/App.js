f785b1576ec3c0da9550b79c194032463b51bf09
DanielAshrafHanna <danielhanna0001@gmail.com>
2020-06-27 17:39:24 +0200

    hey

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import buttons from ".//components/buttons";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <h2>   WIN AS MUCH AS YOU CAN !! </h2>
+          
+        </div>
+        
+        {/* <div class="mt-2 col-md-12">
+        <button type="button" class="btn btn-secondary btn-lg">X         </button>        
+        </div> */}
+
+        {/* <div class="mt-3 col-md-12">
+        <button type="button" class="btn btn-secondary btn-lg">Y        </button>
+         </div> */}
+
+        <Route path="/" exact component={buttons} />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/buttons.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+
+export default class buttons extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          x: 0,
+          y: 0,
+          xy:0, // max number is 4
+          x_score: 0,
+          y_score: 0,
+          show: true
+        };
+      }
+
+      Score= () => {
+        // this.setState({ Y: this.state.Y + 10 });   // for testing 
+
+        if (this.state.x === 1 && this.state.y === 3){
+            // this.setState({ X_score: this.state.X_score + 10 }); 
+            this.setState({x_score: this.state.x_score + 3});
+            this.setState({y_score: this.state.y_score - 1});
+            console.log('x=1 y=3')
+
+   }
+
+        if (this.state.x === 2 && this.state.y === 2){
+            this.setState({x_score: this.state.x_score + 2});
+            this.setState({y_score: this.state.y_score - 2});
+            console.log('x=2 y=2 ')
+        }
+
+        if (this.state.x === 3 && this.state.y === 1){
+            this.setState({x_score: this.state.x_score +1});
+            this.setState({y_score: this.state.y_score -3});
+            console.log('x=3 y=1 ')
+        }
+
+        if (this.state.x === 4 && this.state.y === 0){
+            this.setState({x_score: this.state.y_score -1});
+            console.log('x=4 y=0 ')
+        }
+
+        if (this.state.x === 0 && this.state.y === 4){
+            this.setState({y_score: this.state.y_score +1});
+            console.log('x=0 y=4 ')
+        }
+        
+        }
+
+  IncrementItem_X = (e) => {
+    e.preventDfaule()
+      this.Score() // test
+      this.setState({ x: this.state.x + 1 });
+      this.setState({ xy: this.state.xy + 1 });   // incremeting total score 
+       
+          }
+
+  IncrementItem_Y = (e) => {
+      e.preventDefault()
+    this.Score() // test
+     this.setState({ y: this.state.y + 1 });
+     this.setState({ xy: this.state.xy + 1 });  // incremeting total score 
+     
+          }
+
+
+
+ 
+
+
+
+    render() {
+        return (
+        <div>
+            <div class="mt-3 col-md-12">
+                <button onClick={this.IncrementItem_X} type="button"  class="btn btn-secondary btn-lg">X  </button>
+                { this.state.show ? <h2>{ this.state.x }</h2> : '' }
+                { this.state.show ? <h2>{ this.state.x_score }</h2> : '' }
+                {/* { this.state.show ? <h2>{ this.state.xy }</h2> : '' } */}
+                 
+                
+            </div>
+
+            <div class="mt-3 col-md-12">
+                <button onClick={this.IncrementItem_Y} type="button"  class="btn btn-secondary btn-lg">Y </button>
+                { this.state.show ? <h2>{ this.state.y_score }</h2> : '' }
+                { this.state.show ? <h2>{ this.state.y }</h2> : '' }
+            </div>
+
+                
+         </div>
+            
+            
+            
+            
+            
+            
+        )
+    }
+}
+
